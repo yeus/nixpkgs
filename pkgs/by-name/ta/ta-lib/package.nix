be98a6201dbe0cc2a90ee24844e9f5364bdd035a
Thomas Meschede <yeus@users.noreply.github.com>
2025-01-09 22:30:06 -0800

    Update package.nix
    Update library with new repository URL.
    
    Check the description here:
    
    https://github.com/TA-Lib/ta-lib?tab=readme-ov-file
    
    > "This is now the official home for C/C++ TA-Lib (instead of SourceForge).
    >
    > More info https://ta-lib.org"

--- pkgs/by-name/ta/ta-lib/package.nix
+++ pkgs/by-name/ta/ta-lib/package.nix
@@ -8,12 +8,12 @@
 
 stdenv.mkDerivation rec {
   pname = "ta-lib";
-  version = "0.4.0";
+  version = "0.6.3";
   src = fetchFromGitHub {
-    owner = "rafa-dot-el";
-    repo = "talib";
-    rev = version;
-    sha256 = "sha256-bIzN8f9ZiOLaVzGAXcZUHUh/v9z1U+zY+MnyjJr1lSw=";
+    owner = "ta-lib";
+    repo = "ta-lib";
+    rev = "v0.6.3";
+    sha256 = "sha256-/dBMrZH30htqHcQjMH3x++t/Ktn9P9WKH3OvXFbDDTI=";
   };
 
   nativeBuildInputs = [
